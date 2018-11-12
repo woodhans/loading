2c13367ecfa18999b2e38b8436e34f6054cd3f9b
伍柏川 <wubaichuan@lianluo.com>
2018-11-12 10:26:28 +0800

    增加加载的文件格式

--- jquery.loading.js
+++ jquery.loading.js
@@ -7,58 +7,111 @@
       init: function(el, options) {
         this.$opt = options;
         this.$elem = el;
-        this.beforeLoading && typeof(this.beforeLoading)=="function" &&this.beforeLoading.call();
+        options.beforeLoading && typeof(options.beforeLoading)=="function" &&options.beforeLoading.call();
+        if(options.cancel){
+          timestamps=[];
+          $(document).on("keydown",function(e){
+            if(e.keyCode==27){
+              timestamps.push(new Date().getTime())
+            }
+            if(timestamps.length>=2){
+              timediff=timestamps[timestamps.length-1]-timestamps[timestamps.length-2];
+              if(timediff<2500)
+              this.canceled();
+            }
+          })
+        }
         this._setup();
       },
       _setup: function() {
         var base = this;
-        if (typeof(base.$opt.items) == "string") {
-          base.$opt.items = base.$opt.items.toLowerCase().split("A");
-        }
-        if (Array.isArray(base.$opt.items)) {
+        
+        
           base._generateHtml();
-          n = 0;
-          len = base.$opt.items.length;
-
           
-          for (i = 0; i < len; i++) {
-            _type=base.filetype(base.$opt.items[i]);
-            if (_type) {
-              $.ajax({url:base.$opt.items[i],async:false,success:function(){
-                n++;
-                
-                base.$opt.loadingPercent && base.$elem.find(".loading-percent").html("<span>" + parseInt(n / len * 100, 10) + "</span> %");
-                
-                base.$opt.loadingAnimate && base.$elem.find(".loading-animate").width((n / len * 100) + "%");
-                if (n == len) {
-                  base.remove();
+          // n = 0;
+          // len = base.$opt.items.length;
+        
+          base.loadItem(0);
+          
+          // for (i = 0; i < len; i++) {
+            // _type=base.filetype(base.$opt.items[n]);
+            // if (_type) {
+            //   $.ajax({url:base.$opt.items[i],async:false,success:function(){
+            //     n++;
+            //     console.log(n);
+            //     base.$opt.loadingPercent && base.$elem.find(".loading-percent").html("<span>" + parseInt(n / len * 100, 10) + "</span> %");
+            //     base.$opt.loadingImg && base.$elem.find(".loading-img img").data("percent",(n / len * 100) + "%");
+            //     base.$opt.loadingAnimate && base.$elem.find(".loading-animate").width((n / len * 100) + "%");
+            //     if (n == len) {
+            //       base.remove();
                   
-                  typeof(base.$opt.loaded)=="function" && base.$opt.loaded.call();
-                }
-              },error:function(){
-                n++;
-                console.log("未找到该文件："+base.$opt.items[i]);
-              }})
+            //       typeof(base.$opt.loaded)=="function" && base.$opt.loaded.call();
+            //     }
+            //     return;
+            //   },error:function(){
+            //     n++;
+            //     console.log("未找到该文件："+base.$opt.items[i]);
+            //   }})
               
-            }else {
-              n++;
-              console.log("文件格式不对："+base.$opt.items[i]);
-            }
+            // }else {
+            //   n++;
+            //   console.log("文件格式不对："+base.$opt.items[i]);
+            // }
             
-          }
+          // }
           
 
+        
+      },
+      loadItem:function(loadcounts){
+        var base = this;
+        all=base.$opt.items.length;
+        console.log(loadcounts);
+        if(base.filetype(base.$opt.items[loadcounts])){
+          $.ajax({url:base.$opt.items[loadcounts],async:false,success:function(){
+            loadcounts++;
+            base.loaditem(loadcounts,all);
+          },error:function(){
+            loadcounts++;
+            base.loaditem(loadcounts,all)
+          }})
+        }else{
+          loadcounts++;
+          base.loaditem(loadcounts,all)
         }
       },
-      
-
+      loaditem:function(loadcounts,all){
+        var base=this;
+        base.$opt.loadingPercent && base.$elem.find(".loading-percent").html("<span>" + parseInt(loadcounts / all * 100, 10) + "</span> %");
+            base.$opt.loadingImg && base.$elem.find(".loading-img img").data("percent",parseInt(loadcounts / all * 100) + "%");
+            base.$opt.loadingAnimate && base.$elem.find(".loading-animate").width((loadcounts / all * 100) + "%");
+            
+            if (base.$opt.customAnimate) {
+              base.$elem.find(".loading-custom").data("percent",(loadcounts / all * 100) + "%");
+              if (typeof(base.$opt.customAnimate) == "function") base.$opt.customAnimate.call();
+            }
+            if(loadcounts==all){
+              
+              base.$opt.loadingPercent && base.$elem.find(".loading-percent").html("<span>100</span> %");
+            base.$opt.loadingImg && base.$elem.find(".loading-img img").data("percent","100%");
+            base.$opt.loadingAnimate && base.$elem.find(".loading-animate").width("100%");
+            
+            setTimeout(function(){base.remove();
+              typeof(base.$opt.loaded)=="function" && base.$opt.loaded.call();},10)
+              
+            }else{
+              setTimeout(function(){base.loadItem(loadcounts);},10)
+              
+            }
+      },
       _generateHtml: function() {
         var base = this;
         var html = [];
+        
         html.push("<div class='loading-wrapper'><div class='loading-inner'>");
-
         if (base.$opt.loadingImg)
-          html.push("<div class='loading-img'>" + (typeof(base.$opt.loadingImg) == "string" && base.filetype(base.$opt.loadingImg)=="IMG" ? "<img src='" + base.$opt.loadingImg + "'/>" : "") + "</div>");
+          html.push("<div class='loading-img'><img src='" + (typeof(base.$opt.loadingImg) == "string" && base.filetype(base.$opt.loadingImg)=="IMG" ? "" + base.$opt.loadingImg + "" : "./loading.gif") + " '/></div>");
         if (base.$opt.loadingAnimate)
           html.push("<div class='loading-animate-bg'><div class='loading-animate'></div></div>");
         if (base.$opt.loadingPercent)
@@ -72,22 +125,24 @@
         if (base.$opt.loadingTime) {
           $loadTime = base.$elem.find(".loading-time");
           start = 0;
-          clearTimeout(base.$timeout);
-          base.$timeout = setTimeout(function() {
+          clearInterval(base.$timeout);
+          base.$timeout = setInterval(function() {
             start += .1;
-            $loadTime.html(start + " s");
+            $loadTime.html("<span>"+start.toFixed(1) + "</span> s");
           }, 100);
-        }
-        if (base.$opt.customAnimate) {
-          $custom = base.$elem.find(".loading-custom");
-          if (typeof(base.$opt.customAnimate) == "function") base.$opt.customAnimate.call();
         }
       },
       remove: function() {
         var base = this;
+        clearTimeout();
         if (base.$opt.loadingTime)
-          clearTimeout(base.$timeout);
+          clearInterval(base.$timeout);
         base.$elem.removeClass("loading-body").find('.loading-wrapper').hide().remove();
+      },
+      canceled:function(){
+        var base = this;
+        base.remove();
+        typeof (base.$opt.cancel)=="function" && base.$opt.cancel.call();
       },
       filetype:function(string){
         _type=string.split('.').pop().toLowerCase().split("?")[0];
@@ -119,9 +174,15 @@
   };
   $.fn.loading = function(options) {
     options = $.extend({}, $.fn.loading.options, options);
+    //options.A=options.items;
     if (typeof(options.loaded) != "function") options.loaded = null;
     if (typeof(options.customAnimate) != "function"||typeof(options.customAnimate) != "boolean") options.customAnimate = false;
+    if (typeof(options.items) == "string") options.items = options.toLowerCase().split("A");
+    if (!Array.isArray(options.items))  options.items = [];
+    
+    
     var loads = new Loading();
+    
     loads.init(this, options)
   };
   $.fn.loading.options = {
@@ -132,6 +193,7 @@
     loadingAnimate: true,
     loadingImg: true,
     customAnimate:false,
-    beforeLoading:null
+    beforeLoading:null,
+    cancel:false
   };
 }(jQuery, window, document));
--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 ### author [Hans.Wu](http://www.hanswu.com/project/jquery_loading.html "jQuery.loading")
 ===========================
 
-    一款好用的页面加载动画插件，用来预加载页面图片文件。
+    一款好用的页面预加载动画插件，用来预加载页面外部文件(图片、音频、视频、样式、javascript)。
     可以通过回调和css样式输出各种各样的动画。
     需要使用者掌握一定的css和js, jquery动画编程能力。
 
@@ -33,10 +33,11 @@ $(body).loading({
     loaded: null,               //加载完毕回调函数
     loadingTime: false,         //加载的时间
     loadingPercent: true,       //加载百分比
-    loadingAnimate: true,       //加载百分比进度动画
+    loadingAnimate: true,       //加载百分比进度动画，如果为true，默认loading.gif
     loadingImg: false,          //loading动画gif图片
     customAnimate:false,        //用户自定义动画，可以css或js定义
     beforeLoading:null,         //加载开始前调用函数
+    cancel:false,               //是否可以取消加载动画，如果可以，连按2次esc键，将取消加载动画。值可以是true或者是定义取消后的回调函数
 });
 ```
 ```css
@@ -45,13 +46,12 @@ $(body).loading({
 .loading-inner{}                /* 加载的动画内部显示内容样式 */
 .loading-animate-bg{}           /* 加载的动画百分比进度背景样式 */
 .loading-animate{}              /* 加载的动画百分比进度样式 */
-.loading-img{}                  /* loading动画gif图片样式 */
+.loading-img{}                  /* loading动画图片外层div样式 */
+.loading-img img{}              /* loading动画图片样式 */
 .loading-time{}                 /* 加载的时间显示样式 */
 .loading-percent{}              /* 加载百分比显示样式 */
 .loading-custom{}               /* 用户自定义动画样式 */
 ```
 ## 动画效果Demo
-#### Demo1
-```javascript
 
-```
+
